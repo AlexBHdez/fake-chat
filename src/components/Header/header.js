db0804c0dc57171a7142a1d1ccef1f3cdb0c6e4f
Alex <abhbretones@gmail.com>
2019-03-11 14:19:00 +0100

    UPDATE header styles.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,7 +4,8 @@ import style from './header.module.scss';
 
 const Header = () => (
   <header className={style.header}>
-    <a href="/" className={style.brand} >fakeChat</a>
+    <a href="/" className={style.header__brand} >fakeChat</a>
+    <span className={style.header__copy}>Made with love (and react) by <a href="https://github.com/AlexBHdez/" target="_blank" rel="noopener noreferrer">@AlexBHdez</a></span>
   </header>
 );
 
