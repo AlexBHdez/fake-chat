1c1320f7d18d84da46e3136f1cbfc2985d7e42dc
alexbhdez <abhbretones@gmail.com>
2019-03-11 16:42:57 +0100

    Updates

---
+++ precache-manifest.63bf0d398657b6de395a20b8443a031a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f58507afd45d3b4ec1a",
+    "url": "/AlexBHdez/fake-chat/static/js/runtime~main.a350d21d.js"
+  },
+  {
+    "revision": "7f88fb3638d092c8df06",
+    "url": "/AlexBHdez/fake-chat/static/js/main.d8371ce1.chunk.js"
+  },
+  {
+    "revision": "653ce8636866c453ae7d",
+    "url": "/AlexBHdez/fake-chat/static/js/2.aad716e7.chunk.js"
+  },
+  {
+    "revision": "7f88fb3638d092c8df06",
+    "url": "/AlexBHdez/fake-chat/static/css/main.5beb9454.chunk.css"
+  },
+  {
+    "revision": "8251705d4c26c7b43af26848486968d2",
+    "url": "/AlexBHdez/fake-chat/index.html"
+  }
+];
